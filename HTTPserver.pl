b8a4c3f69d585b29fc69d7c2194d9b7ef02ea558
kiri <ritter@kiri-computer.(none)>
2013-01-31 17:07:24 +0200

    at the begining

--- HTTPserver.pl
+++ HTTPserver.pl
@@ -4,4 +4,11 @@ use strict;
 use IO::Socket;
 use Net::hostent;
 
+my $PORT=8080;
 
+my $server=IO::Socket::INET->new(Proto => 'tcp',
+                              LocalPort => $PORT, 
+                              Listen =>SOMAXCONN, 
+                              Reuse=>1);
+die "can't start server" unless $server;
+print "[Server $0 accepting clients at localhost:$PORT]\n";
