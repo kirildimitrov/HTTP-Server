7c629a283940decd46bbbb2c8feae85d153e9162
kiri <ritter@kiri-computer.(none)>
2013-02-02 18:25:50 +0200

    next try

--- HTTPserver.pl
+++ HTTPserver.pl
@@ -15,8 +15,24 @@ print "[Server $0 accepting clients at localhost:$PORT]\n";
 
 while(my $client=$server->accept()){
   $client->autoflush(1);
-  print $client "Welcome to $0\n";
-  my $hostinfo=gethostbyaddr($client->peeraddr);
-
+  
+  my $request=<$client>;
+  if ($request =~ m|^GET /(.+) HTTP/1.[01]|) {
+    if(-e $1){
+      print $client "HTTP/1.0 200 OK\nContent-Type: text/html\n\n";
+      open(my $f, "<$1");
+      while(<$f>) { print $client $_ };  
+    }else{
+        print $client "HTTP/1.0 404 FILE NOT FOUND\n";
+        print $client "Content-type: text/plain\n\n";
+        print $client "file $1 not found\n";
+      }
+  }else {
+    print $client "HTTP/1.0 400 BAD REQUEST\n";
+    print $client "Content-Type: text/plain\n\n";
+    print $client "BAD REQUEST!\n";
+    print "Client connected\n";   
+   }
+  close $client;
 }
 
