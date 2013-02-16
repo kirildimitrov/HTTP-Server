582a387d6e96dd6ca83f863bdffb4009598aae73
kirildimitrov <ritter@kiri-computer.(none)>
2013-02-16 16:59:55 +0200

    final

--- HTTPserver.pl
+++ HTTPserver.pl
@@ -14,38 +14,62 @@ my $server=IO::Socket::INET->new(Proto => 'tcp',
                               Reuse=>1);
 die "Can't start server" unless $server;
 print "[Server $0 accepting clients at localhost:$PORT]\n";
+my $resolved;
 while(my $client=$server->accept()){
   $client->autoflush(1); 
   my $request=<$client>;
-  
-  if($request =~ m|^POST /(.+) HTTP/1.[01]|){
+  if ($request =~ m|^POST /(.+) HTTP/1.[01]|) {
+    if(-e $1){ 
+      print $client "HTTP/1.0 200 OK\nContent-Type: text/html\n\n";
+      print $client "POST request";
+      print "POST request";
+      $resolved=1;
+    }
+  else{ 
+        print $client "HTTP/1.0 404 FILE NOT FOUND\n";
+        print $client "Content-type: text/plain\n\n";
+        print "file $1 not found\n";
+        print "Can't find this document on the server\n";
+        $resolved=1; 
+        }
+  }
+  if($request =~ m|^PUT /(.+) HTTP/1.[01]|){
     if(-e $1){
       print $client "HTTP/1.0 200 OK\nContent-Type: text/html\n\n";
-      open(my $f, "<$1"); 
-      while($f,"<$1"){ 
-        print $client $_; 
-        print $client "POST method";
-        print "POST Method;"
-      }
+      print $client "PUT request";
+      print "PUT request"; 
+      $resolved=1;
     }
-}  
+      else{
+          print $client "HTTP/1.0 404 FILE NOT FOUND";
+          print $client "Content-type: text/plain\n\n";
+          print "file $1 not found";
+          print "Can't find this document on the server";
+          $resolved=1;
+        }   
+  } 
   if ($request =~ m|^GET /(.+) HTTP/1.[01]|) {
     if(-e $1){
       print $client "HTTP/1.0 200 OK\nContent-Type: text/html\n\n";
+      print "GET request";
       open(my $f, "<$1");
-      while(<$f>) { print $client $_ };  
+      while(<$f>) { print $client $_ };
+      $resolved=1;  
     }else{
         print $client "HTTP/1.0 404 FILE NOT FOUND\n";
         print $client "Content-type: text/plain\n\n";
         print $client "file $1 not found\n";
-        print "Can't find this document on server\n"
+        print "Can't find this document on server\n";
+        $resolved=1;
       }
-  }else {
-    print $client "HTTP/1.0 400 BAD REQUEST\n";
-    print $client "Content-Type: text/plain\n\n";
-    print $client "BAD REQUEST!\n";
-    print "BAD REQUEST\n";   
-   }
+  }else{
+    unless(defined $resolved){
+      print $client "HTTP/1.0 400 BAD REQUEST\n";
+      print $client "Content-Type: text/plain\n\n";
+      print $client "BAD REQUEST!\n";
+      print "BAD REQUEST\n";
+     }   
+  } 
   close $client;
 }
 
